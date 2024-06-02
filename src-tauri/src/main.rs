55d504b5530feaa1f57c2c8fe65cded029b2cb07
Pedro Morales <pedrobmorales@gmail.com>
2024-06-02 17:29:18 -0400

    More progress

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,42 +1,49 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use serde::{Deserialize, Serialize};
 use std::fs;
-use varisat::{dimacs, solver::Solver, Lit};
+use varisat::{cnf, dimacs, solver::Solver, Lit};
 
 const EXTENSION: &str = "cnf";
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct FormulaDetails {
+    num_variables: usize,
+    num_clauses: usize,
+}
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn select_formula(name: &str) -> String {
+fn select_formula() -> FormulaDetails {
     let foo = tauri_api::dialog::select(Some(EXTENSION), Some(".."));
     use tauri_api::dialog::Response::Okay;
     let choice = if let Ok(Okay(file_name)) = foo {
         file_name
     } else {
-        return "File not selected or not found".to_string();
+        return FormulaDetails {
+            num_clauses: 0,
+            num_variables: 0,
+        };
     };
 
     dbg!(&choice);
     let mut solver = Solver::new();
     // let dimacs_cnf = b"1 2 3 0\n-1 -2 0\n-2 -3 0\n";
 
-    let dimacs_cnf = fs::read(choice).unwrap();
+    let dimacs_cnf = fs::read(&choice).unwrap();
     let mut parser = dimacs::DimacsParser::new();
     parser.parse_chunk(dimacs_cnf.as_slice()).unwrap();
     let cnf_formula = parser.take_formula();
 
-    for lits in cnf_formula.iter() {
-        dbg!(&lits);
-    }
-    // dbg!(cnf_formula);
     solver.add_formula(&cnf_formula);
     let mut s2 = Solver::new();
     s2.add_formula(&cnf_formula);
     solver.assume(&[Lit::from_dimacs(-3), Lit::from_dimacs(5)]);
-    let solution = solver.solve().unwrap();
-    let model = solver.model().unwrap();
 
-    format!("{:?}", model)
+    FormulaDetails {
+        num_clauses: cnf_formula.len(),
+        num_variables: cnf_formula.var_count(),
+    }
 }
 
 fn main() {
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,59 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
+import { PrimeReactProvider } from 'primereact/api';
 
+type FormulaDetails = {
+  num_variables: number,
+  num_clauses: number,
+
+
+}
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [formula, setFormula] = useState({ num_variables: 0, num_clauses: 0 } as FormulaDetails);
 
+  function clearFormula() {
+    setFormula({
+      num_clauses: 0,
+      num_variables: 0,
+    });
+
+  }
   async function select_formula() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("select_formula", { name }));
+    setFormula(await invoke("select_formula", {}));
   }
 
+  let content;
+  if (formula.num_clauses != 0) {
+    content = <div>Not null formula</div>
+  } else {
+    content = <div>Yes null formula</div>
+  }
+
+  console.log("Formula", formula);
   return (
-    <div className="container">
-      <h1>SAT Formula Viewer</h1>
+    <PrimeReactProvider>
+      <div className="container">
+        <h1>SAT Formula Viewer</h1>
 
-      <p>Select a formula in DIMACS format with .cnf extension from your file system.</p>
+        <p>Select a formula in DIMACS format with .cnf extension from your file system.</p>
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          select_formula();
-        }}
-      >
-        <button type="submit">Select Formula</button>
-      </form>
+        <form
+          className="row"
+          onSubmit={(e) => {
+            e.preventDefault();
+            select_formula();
+          }}
+        >
+          <button type="submit">Select Formula</button>
+        </form>
+        <button onClick={clearFormula}>Clear Formula</button>
 
-      <p>{greetMsg}</p>
-    </div>
+        <p>{content}</p>
+        <p>Vars: {formula.num_variables} Clauses: {formula.num_clauses}</p>
+      </div>
+    </PrimeReactProvider>
   );
 }
 
