e3a97ccf224d117a11e8fbb6a70acc96128c78fd
Pedro Morales <pedrobmorales@gmail.com>
2024-06-05 09:49:07 -0400

    Making progress in detecting octet map

--- src-tauri/src/expand.rs
+++ src-tauri/src/expand.rs
@@ -13,23 +13,53 @@ struct OctetMapKey {
     var3: isize,
 }
 
+impl OctetMapKey {
+    pub fn index(&self) -> u8 {
+        let mut result = 0;
+        if self.var1 < 0 {
+            result = result | 4;
+        }
+        if self.var2 < 0 {
+            result = result | 2;
+        }
+        if self.var3 < 0 {
+            result = result | 1;
+        }
+        result
+    }
+}
+
 type OctetMap = HashMap<OctetMapKey, u8>;
 
 fn add_to_octet_map(octet_map: &mut OctetMap, clause: &[Lit]) {
     let key = OctetMapKey {
+        var1: clause[0].to_dimacs().abs(),
+        var2: clause[1].to_dimacs().abs(),
+        var3: clause[2].to_dimacs().abs(),
+    };
+    let value = OctetMapKey {
         var1: clause[0].to_dimacs(),
         var2: clause[1].to_dimacs(),
         var3: clause[2].to_dimacs(),
     };
-    dbg!(&key);
+    let index = value.index();
     use std::collections::hash_map::Entry::{Occupied, Vacant};
     let entry = octet_map.entry(key);
     match entry {
         Occupied(mut x) => {
-            *(x.get_mut()) = 10;
+            let val = x.get();
+            println!(
+                "Occupied: val={}, adding index={} val|index={}",
+                val,
+                index,
+                val | index
+            );
+            let bit = 1 << index;
+            *(x.get_mut()) = val | bit;
         }
         Vacant(x) => {
-            x.insert(11);
+            let val = index;
+            x.insert(1 | index);
         }
     }
 }
@@ -48,26 +78,130 @@ mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
 
-    #[test]
-    fn test_octet_map() {
-        let mut formula = CnfFormula::new();
-        let (aP, bP, cP) = (
+    fn get_vars() -> (Lit, Lit, Lit, Lit, Lit, Lit) {
+        let (var1_positive, var2_positive, var3_positive) = (
             Lit::from_dimacs(1),
             Lit::from_dimacs(2),
             Lit::from_dimacs(3),
         );
-        let (aN, bN, cN) = (
+        let (var1_negative, var2_negative, var3_negative) = (
             Lit::from_dimacs(-1),
             Lit::from_dimacs(-2),
             Lit::from_dimacs(-3),
         );
-        dbg!(&aP, &aN);
-        // formula.add_clause(&[aP, bP, cP]);
-        formula.add_clause(&[aP, bP, cN]);
-        formula.add_clause(&[aP, bN, cP]);
-        // formula.add_clause(&[aP, bN, cN]);
+        return (
+            var1_positive,
+            var2_positive,
+            var3_positive,
+            var1_negative,
+            var2_negative,
+            var3_negative,
+        );
+    }
+    #[test]
+    fn test_octet_map() {
+        let (
+            var1_positive,
+            var2_positive,
+            var3_positive,
+            var1_negative,
+            var2_negative,
+            var3_negative,
+        ) = get_vars();
 
-        let octet_map = get_octet_map(formula);
-        assert_eq!(1, octet_map.len());
+        for i in 1..255 {
+            println!("Loop iteration: {}", i);
+            let mut formula: CnfFormula = CnfFormula::new();
+
+            if i & 1 == 1 {
+                formula.add_clause(&[var1_positive, var2_positive, var3_positive]);
+            }
+            if i & 2 == 1 {
+                formula.add_clause(&[var1_positive, var2_positive, var3_negative]);
+            }
+            if i & 4 == 1 {
+                formula.add_clause(&[var1_positive, var2_negative, var3_positive]);
+            }
+            if i & 8 == 1 {
+                formula.add_clause(&[var1_positive, var2_negative, var3_negative]);
+            }
+            if i & 16 == 1 {
+                formula.add_clause(&[var1_negative, var2_positive, var3_positive]);
+            }
+            if i & 32 == 1 {
+                formula.add_clause(&[var1_negative, var2_positive, var3_negative]);
+            }
+            if i & 64 == 1 {
+                formula.add_clause(&[var1_negative, var2_negative, var3_positive]);
+            }
+            if i & 128 == 1 {
+                formula.add_clause(&[var1_negative, var2_negative, var3_negative]);
+            }
+
+            let octet_map = get_octet_map(formula);
+            assert_eq!(1, octet_map.len());
+            let value = octet_map.iter().nth(0).unwrap();
+            let num = value.1;
+            assert_eq!(i, *num);
+        }
+    }
+
+    #[test]
+    fn test_octet_index() {
+        let key = OctetMapKey {
+            var1: 1,
+            var2: 2,
+            var3: 3,
+        };
+        assert_eq!(0, key.index());
+
+        let key = OctetMapKey {
+            var1: 1,
+            var2: 2,
+            var3: -3,
+        };
+        assert_eq!(1, key.index());
+
+        let key = OctetMapKey {
+            var1: 1,
+            var2: -2,
+            var3: 3,
+        };
+        assert_eq!(2, key.index());
+
+        let key = OctetMapKey {
+            var1: 1,
+            var2: -2,
+            var3: -3,
+        };
+        assert_eq!(3, key.index());
+
+        let key = OctetMapKey {
+            var1: -1,
+            var2: 2,
+            var3: 3,
+        };
+        assert_eq!(4, key.index());
+
+        let key = OctetMapKey {
+            var1: -1,
+            var2: 2,
+            var3: -3,
+        };
+        assert_eq!(5, key.index());
+
+        let key = OctetMapKey {
+            var1: -1,
+            var2: -2,
+            var3: 3,
+        };
+        assert_eq!(6, key.index());
+
+        let key = OctetMapKey {
+            var1: -1,
+            var2: -2,
+            var3: -3,
+        };
+        assert_eq!(7, key.index());
     }
 }
